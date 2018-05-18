c18669ef85638336f56898729c756d6c5b0d1da4
Cameron Lee <cmnlee@ucdavis.edu>
2018-05-17 17:57:54 -0700

    setting up repo; implemented makefile

---
+++ REPORT.md
@@ -0,0 +1,21 @@
+# P4 REPORT
+
+## Phase 0
+
+Todo
+
+## Phase 2
+
+todo
+
+## Testing
+
+todo
+
+## Summary
+
+hmm
+
+## Sources: 
+
+add here
---
+++ libfs/fs.c
@@ -0,0 +1,82 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "disk.h"
+#include "fs.h"
+
+/* TODO: Phase 1 */
+
+int fs_mount(const char *diskname)
+{
+	/* TODO: Phase 1 */
+	return 0;
+}
+
+int fs_umount(void)
+{
+	/* TODO: Phase 1 */
+	return 0;
+}
+
+int fs_info(void)
+{
+	/* TODO: Phase 1 */
+	return 0;
+}
+
+int fs_create(const char *filename)
+{
+	/* TODO: Phase 2 */
+	return 0;
+}
+
+int fs_delete(const char *filename)
+{
+	/* TODO: Phase 2 */
+	return 0;
+}
+
+int fs_ls(void)
+{
+	/* TODO: Phase 2 */
+	return 0;
+}
+
+int fs_open(const char *filename)
+{
+	/* TODO: Phase 3 */
+	return 0;
+}
+
+int fs_close(int fd)
+{
+	/* TODO: Phase 3 */
+	return 0;
+}
+
+int fs_stat(int fd)
+{
+	/* TODO: Phase 3 */
+	return 0;
+}
+
+int fs_lseek(int fd, size_t offset)
+{
+	/* TODO: Phase 3 */
+	return 0;
+}
+
+int fs_write(int fd, void *buf, size_t count)
+{
+	/* TODO: Phase 4 */
+	return 0;
+}
+
+int fs_read(int fd, void *buf, size_t count)
+{
+	/* TODO: Phase 4 */
+	return 0;
+}
+
