872215e5f86f96d5ae808f32decb9f8f068f5330
Raymond Liu <rliu11@pc28.cs.ucdavis.edu>
2018-05-24 16:16:49 -0700

    root struct now grabs the root block data from virtual disk in fs_mount()

--- libfs/fs.c
+++ libfs/fs.c
@@ -31,7 +31,7 @@ struct __attribute__((__packed__)) root {
 
 static struct superblock* sb = NULL;
 static struct fat_block* fat_array = NULL;
-//static struct root* root = NULL;
+static struct root* root_global = NULL;
 
 
 int fs_mount(const char *diskname)
@@ -81,12 +81,31 @@ int fs_mount(const char *diskname)
 	size_t read_counter = 1;
 
 	while (total_fat_counter != 0) {
-		if (block_read(read_counter, fat_array->entries[read_counter-1])) {
+		if (block_read(read_counter, fat_array->entries[read_counter-1]) == -1) {
 			return -1;
 		}
 		read_counter++;
 		--total_fat_counter;
 	}
+
+	//Now we do the same thing for the root_global
+	//almost exactly the same as what we did for the superblock
+	root_global = malloc(sizeof(struct root));
+	if(block_read(sb->root_dir_index,buf) == -1){
+		return -1;
+	}
+
+	root_global->filename = malloc(sizeof(uint8_t)*8); // 8 bytes allocated
+
+	memcpy(root_global->filename, buf, 16);
+	memcpy(&root_global->filesize, (buf+16), 4);
+	memcpy(&root_global->first_db_index, (buf+20), 2);
+
+
+	//Now we do it for the data blocks.
+
+
+
 
 	free(buf);
 	return 0;
@@ -113,10 +132,10 @@ int fs_info(void)
 	printf("rdir_blk=%d\n", sb->root_dir_index);
 	printf("data_blk=%d\n", sb->data_block_index);
 	printf("data_blk_count=%d\n", sb->total_data_blocks);
-	
-	printf("fat_free_ratio=%d/%d\n", 
+
+	printf("fat_free_ratio=%d/%d\n",
 		   sb->total_data_blocks-1, sb->total_data_blocks);
-	
+
 	printf("rdir_free_ratio=%d/%d\n",
 		   FS_FILE_MAX_COUNT, FS_FILE_MAX_COUNT); // consider not hardcoding
 
